80fe95426682d5d79265c29877ce86fb7c8ba20f
Mrgrandgrind <matthewaitchie@gmail.com>
2017-03-02 20:56:58 +0000

    First Commit

---
+++ VoxelPunter_01/Assets/VRTK/Examples/Resources/Scripts/Archery/ArrowNotch.cs
@@ -0,0 +1,46 @@
+﻿namespace VRTK.Examples.Archery
+{
+    using UnityEngine;
+
+    public class ArrowNotch : MonoBehaviour
+    {
+        private GameObject arrow;
+        private VRTK_InteractableObject obj;
+
+        private void Start()
+        {
+            arrow = transform.FindChild("Arrow").gameObject;
+            obj = GetComponent<VRTK_InteractableObject>();
+        }
+
+        private void OnTriggerEnter(Collider collider)
+        {
+            var handle = collider.GetComponentInParent<BowHandle>();
+
+            if (handle != null && obj != null && handle.aim.IsHeld() && obj.IsGrabbed())
+            {
+                handle.nockSide = collider.transform;
+                arrow.transform.parent = handle.arrowNockingPoint;
+
+                CopyNotchToArrow();
+
+                collider.GetComponentInParent<BowAim>().SetArrow(arrow);
+                Destroy(gameObject);
+            }
+
+			//arrows stick
+			//if (collider.gameObject.tag == "Enviroment") {
+			//	GetComponent<Rigidbody> ().isKinematic = true;
+			//
+
+        }
+
+        private void CopyNotchToArrow()
+        {
+            GameObject notchCopy = Instantiate(gameObject, transform.position, transform.rotation) as GameObject;
+            notchCopy.name = name;
+            arrow.GetComponent<Arrow>().SetArrowHolder(notchCopy);
+            arrow.GetComponent<Arrow>().OnNock();
+        }
+    }
+}
